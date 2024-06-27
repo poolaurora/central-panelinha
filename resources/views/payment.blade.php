624d4b5a328267a12ad50d636577ef1a285b3d4e
dnkzineo <pamelatomazbotmines@gmail.com>
2024-06-27 18:47:15 -0300

    Update link system

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
             {{ __('Gerar Pagamento e Cadastrar Empresa') }}
         </h2>
     </x-slot>
@@ -8,10 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center space-x-6">
             <!-- Formulário para Gerar Link de Pagamento -->
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 w-1/2">
-                <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Gerar Pagamento</h3>
+            <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 w-1/2">
+                <h3 class="text-lg font-medium text-gray-100 mb-4">Gerar Pagamento</h3>
                 @if ($errors->any())
-                    <div class="mb-4 p-4 bg-red-100 text-red-800 rounded dark:bg-red-900 dark:text-red-200">
+                    <div class="mb-4 p-4 bg-red-900 text-red-200 rounded">
                         <ul class="list-disc pl-5 space-y-1">
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
@@ -23,18 +23,18 @@
                 <form action="{{ route('payments.create') }}" method="POST">
                     @csrf
                     <div class="mb-4">
-                        <label for="amount" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Valor</label>
+                        <label for="amount" class="block text-sm font-medium text-gray-300">Valor</label>
                         <input type="text" name="amount" id="amount" class="mt-1 block text-white w-full bg-gray-900 rounded-md border-gray-700 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required oninput="formatCurrency(this)">
                     </div>
 
                     <div class="mb-4">
-                        <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Descrição</label>
+                        <label for="description" class="block text-sm font-medium text-gray-300">Descrição</label>
                         <input type="text" id="description" name="description" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="gateway" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Gateway</label>
+                        <label for="gateway" class="block text-sm font-medium text-gray-300">Gateway</label>
                         <select name="gateway" id="gateway" class="mt-1 block w-full bg-gray-900 rounded-md border-gray-700 text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
                             <option value="" disabled selected>Selecione o gateway</option>
                             @foreach($empresas as $empresa)
@@ -53,10 +53,10 @@
             </div>
 
             <!-- Formulário para Cadastrar Empresa -->
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 w-1/2">
-                <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Cadastrar Empresa</h3>
+            <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 w-1/2">
+                <h3 class="text-lg font-medium text-gray-100 mb-4">Cadastrar Empresa</h3>
                 @if ($errors->any())
-                    <div class="mb-4 p-4 bg-red-100 text-red-800 rounded dark:bg-red-900 dark:text-red-200">
+                    <div class="mb-4 p-4 bg-red-900 text-red-200 rounded">
                         <ul class="list-disc pl-5 space-y-1">
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
@@ -68,33 +68,33 @@
                 <form action="{{ route('payments.store') }}" method="POST">
                     @csrf
                     <div class="mb-4">
-                        <label for="company_name" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Nome da Empresa</label>
+                        <label for="company_name" class="block text-sm font-medium text-gray-300">Nome da Empresa</label>
                         <input type="text" name="company_name" id="company_name" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="api_key" class="block text-sm font-medium text-gray-700 dark:text-gray-300">API Key</label>
+                        <label for="api_key" class="block text-sm font-medium text-gray-300">API Key</label>
                         <input type="text" name="api_key" id="api_key" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="api_secret" class="block text-sm font-medium text-gray-700 dark:text-gray-300">API Secret</label>
+                        <label for="api_secret" class="block text-sm font-medium text-gray-300">API Secret</label>
                         <input type="text" name="api_secret" id="api_secret" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="return_url" class="block text-sm font-medium text-gray-700 dark:text-gray-300">URL de Retorno</label>
+                        <label for="return_url" class="block text-sm font-medium text-gray-300">URL de Retorno</label>
                         <input type="text" name="return_url" id="return_url" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="cancel_url" class="block text-sm font-medium text-gray-700 dark:text-gray-300">URL de Cancelamento</label>
+                        <label for="cancel_url" class="block text-sm font-medium text-gray-300">URL de Cancelamento</label>
                         <input type="text" name="cancel_url" id="cancel_url" required
-                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            class="mt-1 block w-full rounded-md border-gray-700 shadow-sm bg-gray-900 text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
@@ -109,12 +109,19 @@
     </div>
 
     <script>
-        function formatCurrency(input) {
-            let value = input.value.replace(/\D/g, '');
-            value = (value / 100).toFixed(2) + '';
-            value = value.replace(".", ".");
-            value = value.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-            input.value = value;
-        }
-    </script>
+    function formatCurrency(input) {
+        // Remove todos os caracteres que não sejam números
+        let value = input.value.replace(/\D/g, '');
+        
+        // Converte o valor para centavos
+        value = (value / 100).toFixed(2) + '';
+        
+        // Substitui a vírgula decimal por um ponto (se houver)
+        value = value.replace(",", ".");
+        
+        // Atualiza o valor do input
+        input.value = value;
+    }
+</script>
+
 </x-app-layout>
