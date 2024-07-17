1e1b235efdc942e72953f7b7cb0012e1856296c1
dnkzineo <pamelatomazbotmines@gmail.com>
2024-07-17 11:38:56 -0300

    PDF FULL LOADED

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -81,8 +81,19 @@ private function gerarTransacoes($periodo, $min_transacoes, $saldo_inicial, $fat
     $end_date = Carbon::now();
 
     // Calcular a faixa de 20 a 30% do faturamento
-    $min_faturamento = $faturamento * 2.0;
-    $max_faturamento = $faturamento * 2.0;
+    if ($min_transacoes <= 1000) {
+        $min_faturamento = $faturamento * 2.0;
+        $max_faturamento = $faturamento * 2.0;
+    } elseif ($min_transacoes <= 2000) {
+        $min_faturamento = $faturamento * 3.0;
+        $max_faturamento = $faturamento * 3.0;
+    } elseif ($min_transacoes <= 3000) {
+        $min_faturamento = $faturamento * 4.0;
+        $max_faturamento = $faturamento * 4.0;
+    } elseif ($min_transacoes <= 4000) {
+        $min_faturamento = $faturamento * 5.0;
+        $max_faturamento = $faturamento * 5.0;
+    }    
     $faturamento_distribuir = rand($min_faturamento, $max_faturamento);
 
     // Gerar transações e calcular saldo inicial
